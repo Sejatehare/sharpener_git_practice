bdd77c2bf4f0dcf6bc795f99224970d78e8308e7
Sejatehare <sejaltehare3@gmail.com>
2024-06-23 17:52:01 +0530

    Todos updated

--- Todo Rest Api/index.js
+++ Todo Rest Api/index.js
@@ -1,47 +1,46 @@
-const cors_proxy = "http://localhost:8080/";
-const api_url = "https://crudcrud.com/api/0ca0968f98374ebe979351144a7e0957/todos";
+const crud_url = "https://crudcrud.com/api/91e8ee5e2f6542a0997d4498faa718b4/todos";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded",() => {
     axios
-    .get(`${cors_proxy}${api_url}`)
+    .get(`${crud_url}`)
     .then((response) => {
-        response.data.forEach((obj) => {
-            addTodoToList(obj);
+        response.data.forEach((obj) =>{
+            addTolist(obj);
         });
     })
     .catch((error) => console.log(error))
 });
 
-function formsubmit(event){
+
+function fomsubmitted(event){
     event.preventDefault();
 
-    const todoName = event.target.name.value;
+    const name = event.target.todoName.value;
     const desc = event.target.desc.value;
-    const status = false;
+    let status = false;
 
-    const obj = { todoName, desc, status};
-
+    const obj = { name, desc, status};
     axios
-    .post(`${cors_proxy}${api_url}`,obj)
+    .post(`${crud_url}`,obj)
     .then((response) => {
-        addTodoToList(response.data)
+        addTolist(response.data);
     })
     .catch((error) => console.log(error))
     event.target.reset();
 }
 
-function addTodoToList(obj) {
-    const newLi = document.createElement("li");
+
+
+function addTolist(obj) {
+    const newLi = document.createElement('li');
     newLi.appendChild(
-        document.createTextNode(
-            `${obj.todoName} - ${obj.desc} - `
-        )
+        document.createTextNode(`${obj.name} - ${obj.desc} - `)
     );
 
     const donebox = document.createElement("input");
     donebox.setAttribute("type","checkbox");
     const labelDone = document.createElement("label");
-    labelDone.appendChild(document.createTextNode("DONE  / ."));
+    labelDone.appendChild(document.createTextNode("DONE -"));
     newLi.appendChild(donebox);
     newLi.appendChild(labelDone);
 
@@ -52,45 +51,54 @@ function addTodoToList(obj) {
     newLi.appendChild(notdonebox);
     newLi.appendChild(labelPend);
 
-    const pendinglist = document.getElementById("pendinglist");
+    const pendingList = document.getElementById("pendinglist");
     const donelist = document.getElementById("donelist");
 
-    if (obj.status){
-        donelist.appendChild(newLi)
+    if (obj.status) {
+        donelist.appendChild(newLi);
     }
     else{
-        pendinglist.appendChild(newLi);
+        pendingList.appendChild(newLi);
     }
 
-    donebox.addEventListener("click", function() {
-        obj.status = true;
-        updatetodo(obj,newLi);
+    donebox.addEventListener("change", function(){
+        updateTodo(obj);
+    });
+    notdonebox.addEventListener("change", function(){
+        updateTodo(obj);
     });
 
-    notdonebox.addEventListener("click", function() {
-        obj.status = false;
-        updatetodo(obj,newLi);
-    })
+    checkPendingList(pendingList);
+
 }
 
-function updatetodo (obj, listItem){
+function updateTodo (obj){
     axios
-        .put(`${cors_proxy}${api_url}/${obj._id}`,obj)
-        .then(() => {
-            const donelist = document.getElementById("donelist");
-            const pendinglist = document.getElementById("pendinglist");
-            if (obj.status) {
-                donelist.appendChild(listItem);
-                if (pendinglist.contains(listItem)) {
-                    pendinglist.removeChild(listItem);
-                }
-            } else {
-                pendinglist.appendChild(listItem);
-                if (donelist.contains(listItem)) {
-                    donelist.removeChild(listItem);
-                }
-            }
+        .put(`${crud_url}/${obj._id}`,{
+            name: obj.name,
+            desc: obj.desc,
+            status: !obj.status
         })
-        .catch((error) => console.log(error));
+        .then((response) => console.log(response.data))
+        .catch((error) => console.log(error))
+    
+}
 
+
+function checkPendingList(pendingList) {
+    const msg = document.createElement('p');
+    msg.textContent = "YUPP! No work to complete...";
+    msg.id = "msg";
+
+    const msgexist = document.getElementById("msg");
+
+    if (pendingList.getElementsByTagName('li').length === 0) {
+        if (!msgexist) {
+            pendingList.appendChild(msg);
+        }
+    } else {
+        if (msgexist) {
+            msgexist.remove();
+        }
+    }
 }
